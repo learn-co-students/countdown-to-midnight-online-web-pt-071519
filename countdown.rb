702b6ed8801eaa05824853f8ac1426f880070675
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-20 22:23:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
-
+countdown = 10 
+while countdown <= 10 do 
+  puts "#{countdown} SECOND(S)!"
+  countdown -= 1 
+if countdown == 0
+    break
+end
+end
 def countdown
+  x == 0
+puts "HAPPY NEW YEAR!"
 end
