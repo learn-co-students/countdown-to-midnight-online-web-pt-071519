4b98ca11c5b5f1d62c191b19b62e5c67409ab128
David Jermaine Whitehead <david_whitehead_234@yahoo.com>
2019-07-22 20:25:07 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,31 @@
 #write your code here
+require "pry"
 
-def countdown
+def countdown(number)
+ 
+ countdown_output = 10
+ #puts countdown_output <= 10
+ 
+    while countdown_output <= 10
+   puts "#{countdown_output} SECOND(S)!"
+   countdown_output -= 1
+   break if countdown_output == 0
+ end
+ return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(number)
+
+countdown_output = 10
+ #puts countdown_output <= 10
+ 
+  while countdown_output <= 10
+   puts "#{countdown_output} SECOND(S)!"
+   countdown_output -= 1
+   sleep(5)
+   break if countdown_output == 0
+   
+  end
+ return "HAPPY NEW YEAR!"
 end
