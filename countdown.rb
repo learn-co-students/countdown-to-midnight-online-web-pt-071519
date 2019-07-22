25b2944df08f8ae8231456fca80fc1c140960763
Jarred Wilkins <wilkins.jarred@outlook.com>
2019-07-22 03:12:32 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  x=10
+  while x > 0
+    puts "#{x} SECOND(S)!"
+  x-= 1
+  end
 end
+
+
+
+    def countdown_with_sleep(x)
+        x= sleep 1
+        while x < 5
+        puts "HAPPY NEW YEAR!"
+        x-= 1
+      end
+    end 
+  
