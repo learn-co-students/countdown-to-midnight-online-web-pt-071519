f7b11b9aa1f3af0fe568f75c42dd84d6f688cdcd
jose luis <cortezluisjose@gmail.com>
2019-07-23 01:16:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
-
-def countdown
+def countdown(10)
+ countdown_output = 10 
+while countdown_output < 12
+puts runtime = Benchmark.measure { countdown_with_sleep(5) }
+puts "HAPPY NEW YEAR!"
+countdown_output -= 1
+ end
 end
